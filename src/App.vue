d900a78a8d7e412270bd366ad070ddea4f7ce26f
LiZi35 <thelizi35@163.com>
2026-05-05 19:16:13 +0800

    基本框架

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="mainPage">
+        <p>main</p>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.mainPage {
+    width: 80%;
+    height: 80%;
+    border: 1px solid var(--el-border-color);
+    border-radius: var(--el-border-radius-base);
+    box-shadow: var(--el-box-shadow-dark);
+}
+</style>
